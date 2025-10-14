106bcadf31652664408e3a13592e9d1fd8455a16
KatsumiOuwa <d2p.yggdrasill@gmail.com>
2025-10-14 23:03:38 +0900

    Improve toolchain bootstrap handling

--- cmake/ToolchainBootstrap.cmake
+++ cmake/ToolchainBootstrap.cmake
@@ -12,8 +12,25 @@ set(LLM630_TOOLCHAIN_URL
     "https://developer.arm.com/-/media/Files/downloads/gnu-a/9.2-2019.12/binrel/${LLM630_TOOLCHAIN_VERSION}.tar.xz"
 )
 
-if(NOT EXISTS "${LLM630_TOOLCHAIN_DIR}/bin/aarch64-none-linux-gnu-g++")
+set(_llm630_toolchain_cxx "${LLM630_TOOLCHAIN_DIR}/bin/aarch64-none-linux-gnu-g++")
+set(_llm630_toolchain_c "${LLM630_TOOLCHAIN_DIR}/bin/aarch64-none-linux-gnu-gcc")
+
+set(_llm630_need_toolchain FALSE)
+if(NOT EXISTS "${_llm630_toolchain_cxx}" OR NOT EXISTS "${_llm630_toolchain_c}")
+    set(_llm630_need_toolchain TRUE)
+endif()
+
+if(_llm630_need_toolchain)
     file(MAKE_DIRECTORY "${LLM630_TOOLCHAIN_ROOT}")
+
+    # Drop invalid cached archives so we always re-fetch a usable toolchain.
+    if(EXISTS "${LLM630_TOOLCHAIN_ARCHIVE}")
+        file(SIZE "${LLM630_TOOLCHAIN_ARCHIVE}" _llm630_archive_size)
+        if(_llm630_archive_size LESS 1024)
+            message(STATUS "Discarding invalid cached toolchain archive (${LLM630_TOOLCHAIN_ARCHIVE})")
+            file(REMOVE "${LLM630_TOOLCHAIN_ARCHIVE}")
+        endif()
+    endif()
 
     if(NOT EXISTS "${LLM630_TOOLCHAIN_ARCHIVE}")
         message(STATUS "Downloading ARM GNU toolchain (${LLM630_TOOLCHAIN_VERSION}) ...")
@@ -21,9 +38,25 @@ if(NOT EXISTS "${LLM630_TOOLCHAIN_DIR}/bin/aarch64-none-linux-gnu-g++")
             "${LLM630_TOOLCHAIN_URL}"
             "${LLM630_TOOLCHAIN_ARCHIVE}"
             SHOW_PROGRESS
+            STATUS _llm630_download_status
+            TLS_VERIFY ON
         )
+        list(GET _llm630_download_status 0 _llm630_download_status_code)
+        if(NOT _llm630_download_status_code EQUAL 0)
+            message(FATAL_ERROR "Failed to download toolchain archive: ${_llm630_download_status}")
+        endif()
     else()
         message(STATUS "Using cached toolchain archive: ${LLM630_TOOLCHAIN_ARCHIVE}")
+    endif()
+
+    file(SIZE "${LLM630_TOOLCHAIN_ARCHIVE}" _llm630_archive_size_after)
+    if(_llm630_archive_size_after LESS 1024)
+        file(REMOVE "${LLM630_TOOLCHAIN_ARCHIVE}")
+        message(FATAL_ERROR "Downloaded toolchain archive is invalid (size ${_llm630_archive_size_after} bytes)")
+    endif()
+
+    if(EXISTS "${LLM630_TOOLCHAIN_DIR}")
+        file(REMOVE_RECURSE "${LLM630_TOOLCHAIN_DIR}")
     endif()
 
     message(STATUS "Extracting toolchain to ${LLM630_TOOLCHAIN_ROOT}")
@@ -38,16 +71,16 @@ if(NOT EXISTS "${LLM630_TOOLCHAIN_DIR}/bin/aarch64-none-linux-gnu-g++")
     endif()
 endif()
 
-set(LLM630_TOOLCHAIN_BIN "${LLM630_TOOLCHAIN_DIR}/bin")
-set(LLM630_C_COMPILER "${LLM630_TOOLCHAIN_BIN}/aarch64-none-linux-gnu-gcc")
-set(LLM630_CXX_COMPILER "${LLM630_TOOLCHAIN_BIN}/aarch64-none-linux-gnu-g++")
+if(NOT EXISTS "${_llm630_toolchain_cxx}")
+    message(FATAL_ERROR "Cross compiler not found at ${_llm630_toolchain_cxx}")
+endif()
+if(NOT EXISTS "${_llm630_toolchain_c}")
+    message(FATAL_ERROR "Cross compiler not found at ${_llm630_toolchain_c}")
+endif()
 
-if(NOT EXISTS "${LLM630_C_COMPILER}")
-    message(FATAL_ERROR "Cross compiler not found at ${LLM630_C_COMPILER}")
-endif()
-if(NOT EXISTS "${LLM630_CXX_COMPILER}")
-    message(FATAL_ERROR "Cross compiler not found at ${LLM630_CXX_COMPILER}")
-endif()
+set(LLM630_TOOLCHAIN_BIN "${LLM630_TOOLCHAIN_DIR}/bin")
+set(LLM630_C_COMPILER "${_llm630_toolchain_c}")
+set(LLM630_CXX_COMPILER "${_llm630_toolchain_cxx}")
 
 if(NOT DEFINED CMAKE_SYSTEM_NAME)
     set(CMAKE_SYSTEM_NAME "Linux")
